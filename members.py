0e1674ec1936b2e9f4d840aafa34b2ace5e3733a
Joel <joeldiaz703.jd@gmail.com>
2026-04-30 19:39:56 -0400

    change some logic and styles

--- members.py
+++ members.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from pathlib import Path
+
+st.set_page_config(layout="wide")
 
 a,b = st.columns(2)
-#b.image("Images/logofupu.png",caption="La fuerza del pueblo",width=100)
-st.title("Reporte de miembros")
+b.image("Images/logofupu.png",caption="La fuerza del pueblo",width=100)
+st.title("Reporte de votantes inscritos")
 
 data = st.file_uploader("Sube el archivo excel",type=["xlsx"])
 
@@ -15,10 +18,17 @@ if data is not None:
     "Filtrar por terrotorio",
     options=prov,
     index=None,
-    placeholder="Selecciona una territorio"
+    placeholder="Buscar una territorio"
+)
+   sel2 = st.selectbox(
+    "Filtrar por usuario",
+    options=df["Inscrito por"].unique(),
+    index=None,
+    placeholder="Buscar un usuario"
 )
    df_filter = df[df["Territorio"] == sel]
-   
+   df_filterusers = df[df["Inscrito por"] == sel2]
+
    hombres = (df["Género"] == "M").sum()
    mujeres = (df["Género"] == "F").sum()
    col1,col2,col3 = st.columns(3)
@@ -30,6 +40,13 @@ if data is not None:
     col2.metric(label="Hombres",value=hombres,border=True)
     mujeres = (df_filter["Género"] == "F").sum()
     col3.metric("Mujeres",value=mujeres,border=True)
+   elif sel2:
+       total2 = df_filterusers["Nombre"].count()
+       col1.metric(label="Total de miembros",value=total2,border=True)
+       hombres = (df_filterusers["Género"] == "M").sum()
+       col2.metric(label="Hombres",value=hombres,border=True)
+       mujeres = (df_filterusers["Género"] == "F").sum()
+       col3.metric(label="Mujeres",value=mujeres,border=True)
    else:
      total1 = df["Nombre"].count()
      col1.metric(label="Total de miembros",value=total1,border=True)
@@ -38,12 +55,14 @@ if data is not None:
 
    if sel: 
     st.dataframe(df_filter)
-   else:
+   elif sel2:     
+    st.dataframe(df_filterusers)
+   else:    
     st.dataframe(df)
 
-   miembros_por_usuario = df.groupby("Inscrito por").size().reset_index(name="Total de miembros").sort_values(by="Total de miembros",ascending=False)
-   top5_usuarios = df.groupby("Inscrito por").size().reset_index(name="Miembros por usuarios").sort_values(by="Miembros por usuarios",ascending=True).tail(5)
-   miembros_por_terrotorio = df.groupby("Territorio").size().reset_index(name="Miembros por territorio").sort_values(by="Miembros por territorio",ascending=False)
+   miembros_por_usuario = df.groupby("Inscrito por").size().reset_index(name="Total de votantes").sort_values(by="Total de votantes",ascending=False)
+   top5_usuarios = df.groupby("Inscrito por").size().reset_index(name="Votantes por usuarios").sort_values(by="Votantes por usuarios",ascending=False)
+   miembros_por_terrotorio = df.groupby("Territorio").size().reset_index(name="Votantes por territorio").sort_values(by="Votantes por territorio",ascending=False)
 
 
 
@@ -55,15 +74,15 @@ if data is not None:
 if data is not None:
  fig = px.bar(
     top5_usuarios,
-    x="Miembros por usuarios",
+    x="Votantes por usuarios",
     y="Inscrito por",
-    text="Miembros por usuarios",
-    title="Top 5 usuarios con más miembros",
-    color="Miembros por usuarios"
+    text="Votantes por usuarios",
+    title="Total de votantes por usuarios",
+    color="Votantes por usuarios"
 )
- st.title("Total de miembros por territorios")
+ st.title("Total de votantes por territorios")
  st.dataframe(miembros_por_terrotorio)
- st.title("5 Usuarios con mas miembros")
+ st.title("Votantes por usuarios")
  st.plotly_chart(fig, use_container_width=True)
- st.title("Total de miembros por usuarios")
+ st.title("Total de votantes por usuarios")
  st.dataframe(miembros_por_usuario)
